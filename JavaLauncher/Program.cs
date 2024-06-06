fe7aed40d1e5a7986a13cdd945c0c9dd6833d873
xulihang <xulihanghai@163.com>
2024-06-06 22:02:15 +0800

    init

---
+++ JavaLauncher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JavaLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("jre\\bin\\java", "--module-path jre\\javafx\\lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.web,javafx.swing --add-opens javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar .\\ImageTrans.jar");
+            startInfo.UseShellExecute = false; // 不使用操作系统外壳程序启动
+            startInfo.CreateNoWindow = true; // 不创建新窗口
+            try
+            {
+                Process process = Process.Start(startInfo);
+                process.WaitForExit(); // 如果需要，等待程序执行完成
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("发生错误: " + ex.Message);
+            }
+        }
+    }
+}
