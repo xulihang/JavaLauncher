81ddb2572e9bd8fdde16d857864fbf6f058612b6
xulihang <xulihanghai@163.com>
2025-01-27 13:39:19 +0800

    adapt for silhouette

--- JavaLauncher/Program.cs
+++ JavaLauncher/Program.cs
@@ -7,7 +7,7 @@ namespace JavaLauncher
     {
         static void Main(string[] args)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo("jre\\bin\\java", "--module-path jre\\javafx\\lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.web,javafx.swing --add-opens javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar .\\ImageTrans.jar");
+            ProcessStartInfo startInfo = new ProcessStartInfo("jre\\bin\\java", "--module-path jre\\javafx\\lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.web,javafx.swing --add-opens javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-opens javafx.graphics/com.sun.glass.ui=ALL-UNNAMED .\\Silhouette.jar");
             startInfo.UseShellExecute = false; // 不使用操作系统外壳程序启动
             startInfo.CreateNoWindow = true; // 不创建新窗口
             try
