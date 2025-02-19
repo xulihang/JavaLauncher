e8eb46278081cac2caa19926565f8d647ff26b02
xulihang <xulihanghai@163.com>
2025-02-19 20:34:16 +0800

    set working dir

--- JavaLauncher/Program.cs
+++ JavaLauncher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace JavaLauncher
 {
@@ -7,6 +8,7 @@ namespace JavaLauncher
     {
         static void Main(string[] args)
         {
+            string workDir = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString();
             string arguments = "--module-path jre\\javafx\\lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.web,javafx.swing --add-opens javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar .\\ImageTrans.jar";
             if (args.Length > 0) {
                 arguments += " ";
@@ -15,6 +17,7 @@ namespace JavaLauncher
             ProcessStartInfo startInfo = new ProcessStartInfo("jre\\bin\\java", arguments);
             startInfo.UseShellExecute = false; // 不使用操作系统外壳程序启动
             startInfo.CreateNoWindow = true; // 不创建新窗口
+            startInfo.WorkingDirectory = workDir;
             try
             {
                 Process process = Process.Start(startInfo);
