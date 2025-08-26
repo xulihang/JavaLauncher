0d1eca1316b1fd8eadb886d23e99ca577f25b25d
xulihang <xulihanghai@163.com>
2025-08-26 13:42:28 +0800

    set target framework to 2.0

--- JavaLauncher/Program.cs
+++ JavaLauncher/Program.cs
@@ -8,17 +8,24 @@ namespace JavaLauncher
     {
         static void Main(string[] args)
         {
-            string workDir = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString();
+            string workDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             string arguments = "--module-path jre\\javafx\\lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.web,javafx.swing --add-opens javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-opens javafx.graphics/com.sun.glass.ui=ALL-UNNAMED .\\Silhouette.jar";
+
             if (args.Length > 0)
             {
                 arguments += " ";
-                arguments += String.Join(" ", args);
+                foreach (string arg in args)
+                {
+                    arguments += "\"" + arg + "\" ";
+                }
+                arguments = arguments.TrimEnd(); // 移除末尾多余的空格
             }
+
             ProcessStartInfo startInfo = new ProcessStartInfo("jre\\bin\\java", arguments);
             startInfo.UseShellExecute = false; // 不使用操作系统外壳程序启动
             startInfo.CreateNoWindow = true; // 不创建新窗口
             startInfo.WorkingDirectory = workDir;
+
             try
             {
                 Process process = Process.Start(startInfo);
